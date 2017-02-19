e286f1fc43fc21169e2665ddfe2090b3e0cff8f6
Kris <krzysztof.surowiecki@gmail.com>
2017-02-19 20:38:01 +0100

    new files / updates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    metaTitle = 'Hexe Data. Analityka Internetowa'
+    context = {
+        'metaTitle' : metaTitle,
+    }
+    return render(request, 'index.html', context)
+
+
+def oferta(request):
+    return render(request, 'oferta.html')
+
+
+def kompetencje(request):
+    return render(request, 'kompetencje.html')
+
+
+def kontakt(request):
+    return render(request, 'kontakt.html')
+
+
