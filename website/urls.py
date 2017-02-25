4539659efbe1f548b98d8d59a1fd3c15e9935925
Kris <krzysztof.surowiecki@gmail.com>
2017-02-25 21:51:11 +0100

    add

--- website/urls.py
+++ website/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^kompetencje/', views.kompetencje, name='kompetencje'),
-    url(r'kontakt/', views.kontakt),
+    url(r'kontakt/', views.kontakt, name='kontakt'),
     url(r'oferta/', views.oferta, name='oferta'),
     url(r'onas/', views.oferta),
     url(r'index/', views.index),
     url(r'narzedzia/', views.narzedzia),
     url(r'newsroom/', views.newsroom),
-    url(r'analiza-danych/', views.analizaDanych),
-    url(r'visual-analytics/', views.visualAnalytics),
+    url(r'analiza-danych/', views.analizaDanych, name='analizaDanych'),
+    url(r'visual-analytics/', views.visualAnalytics, name='visualAnalytics'),
 ]
